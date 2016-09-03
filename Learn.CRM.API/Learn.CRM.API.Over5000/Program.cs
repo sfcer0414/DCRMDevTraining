e1d63eeaf3950261081efa7fb0c78ae78ef5f353
Jeremy Lin <sfcer0414@hotmail.com>
2016-09-03 10:00:37 +0800

    建立專案結構

---
+++ Learn.CRM.API/Learn.CRM.API.Over5000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.CRM.API.Over5000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
